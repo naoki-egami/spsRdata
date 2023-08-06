696b3db0c50b82ea32311d7bef167ad2b9e30840
DianaDaInLee <dl2860@columbia.edu>
2023-08-06 16:52:58 -0500

    Add data

---
+++ R/data.R
@@ -0,0 +1,13 @@
+#' SPS site selection data
+#' @name sps_data
+#' @docType data
+#' @format data
+#' @keywords datasets
+NULL
+
+#' Codebook for sps_data
+#' @name codebook
+#' @docType data
+#' @format data
+#' @keywords codebook for sps_data
+NULL
