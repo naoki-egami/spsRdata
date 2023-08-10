41ea27e4deaf13d6478c901f9b01d556aae3fec6
DianaDaInLee <dl2860@columbia.edu>
2023-08-10 00:58:39 -0500

    up

--- R/merge_data.R
+++ R/merge_data.R
@@ -43,24 +43,32 @@ merge_data <- function(basedata, newdata, id_year = NULL, id_country = NULL, iso
                              distance_col = "distance",
                              mode = "inner",
                              max_dist = 20,
-                             ...) %>%
-      dplyr::group_by_at(c('cmatch.y', id_year)) %>%
-      dplyr::slice_min(order_by = .data[['distance']], n = 1)
-    fuzzy <- fuzzy[, c('iso3', 'cmatch.x', id_country, id_year, newvars)]
-    names(fuzzy) <- c('iso3', 'country', 'cname_used', id_year, newvars)
+                             ...)
+    fuzzy <- dplyr::group_by_at(fuzzy, c('cmatch.y', id_year))
+    fuzzy <- dplyr::slice_min(fuzzy, order_by = .data[['distance']], n = 1, with_ties = FALSE)
+    if (max(fuzzy$distance)>0){
+      warning(paste('Following countries were matched using fuzzy-match:\n',
+                    paste(paste(fuzzy$cmatch.y[fuzzy$distance>0], '=>', fuzzy$cmatch.x[fuzzy$distance>0]), collapse = '\n '),
+                    '\nPlease review and if necessary, recode the country names in your dataset or supply the ISO3 code instead.'))
+    }
+    # fuzzy <- fuzzy[, c('iso3', 'cmatch.x', id_country, id_year, newvars)]
+    # names(fuzzy) <- c('iso3', 'country', 'cname_used', id_year, newvars)
+    fuzzy <- fuzzy[, c('iso3', 'cmatch.x', id_year, newvars)]
+    names(fuzzy) <- c('iso3', 'country', id_year, newvars)
 
     fuzzy$merge  <- 'fuzzy'
     newdata$merge <- 'exact'
     xcol <- c('year', 'country')
     if (is.null(id_year)) xcol <- c('country')
     data0 <- merge(basedata, newdata[,c(id_country, id_year, newvars, 'merge')], by.x = xcol, by.y = c(id_year, id_country), all.x = TRUE)
-    data1 <- merge(data0, fuzzy, by.x = c('year', 'iso3', 'country'), by.y = c(id_year, 'iso3', 'country'), all.x = TRUE, suffixes = c('', '.f'))
+    data1 <- merge(data0, fuzzy, by.x = c(xcol, 'iso3'), by.y = c(id_year, 'country', 'iso3'), all.x = TRUE, suffixes = c('', '.f'))
     data1$match <- ifelse(is.na(data1$merge), data1$merge.f, data1$merge)
-    data1$cname_used <- ifelse(data1$match == 'exact', data1$country, data1$cname_used)
+    # data1$cname_used <- ifelse(data1$match == 'exact', data1$country, data1$cname_used)
     for (i in newvars){
       data1[[i]] <- ifelse(is.na(data1$match), NA, ifelse(data1$match == 'exact', data1[[i]], data1[[paste0(i, '.f')]]))
     }
-    data <- data1[,c(names(basedata), newvars, 'match', 'cname_used')]
+    # data <- data1[,c(names(basedata), newvars, 'match', 'cname_used')]
+    data <- data1[,c(names(basedata), newvars)]
   }
   return(data)
 }
