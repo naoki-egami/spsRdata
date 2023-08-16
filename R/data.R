2bb151de4c5bcb835205c1beb0bfb601c9f60b6a
DianaDaInLee <dl2860@columbia.edu>
2023-08-15 21:50:56 -0500

    up

--- R/data.R
+++ R/data.R
@@ -1,11 +1,11 @@
 #' SPS country-level site selection data
-#' @name sps_country_data_full
+#' @name sps_country_data
 #' @docType data
 #' @format data
 #' @keywords datasets
 NULL
 
-#' Codebook for sps_country_data_full
+#' Codebook for sps_country_data
 #' @name sps_country_codebook
 #' @docType data
 #' @format data
